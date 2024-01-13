c0bab7be4fd3d72dd57bd0c39b6de8ac57bd80dd
alcatraz47 <mahmudulhaquearfan@gmail.com>
2024-01-13 22:01:00 +0100

    data encoder added in file reader

--- api/functions/file_reader.py
+++ api/functions/file_reader.py
@@ -1,8 +1,28 @@
 import pandas as pd
+import magic
 from io import StringIO
 from xlsx2csv import Xlsx2csv
 
 ac_ext = ["csv", "xlsx"]
+
+def detect_encoding(file_blob: object) -> str:
+    """detects encoding of few characters of file
+    as blob. If no encoding is found attempted
+    cp1252. retunrs the encoding name.
+
+    Args:
+        file_blob (object): few characters of file content
+
+    Returns:
+        str: encoding of the file
+    """
+    m = magic.open(magic.MAGIC_MIME_ENCODING)
+    m.load()
+    encoding = m.buffer(file_blob)
+    if encoding=="unknown-8bit":
+        return "cp1252"
+    return encoding
+
 def read_file(file_object: object) -> pd.DataFrame:
     return pd.read_csv(file_object, encoding="utf-8")
     # try:
